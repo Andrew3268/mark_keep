8973298c7898a8f2ea233b1bf15f463beb66d388
Your <scarlet3628@likelion.org>
2019-09-01 10:56:56 +0900

    image Upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ApplicationRecord
   
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  has_attached_file :image, styles: { medium: "600x500#", small: "350x250>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190901012039) do
+ActiveRecord::Schema.define(version: 20190901015357) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -44,6 +44,10 @@ ActiveRecord::Schema.define(version: 20190901012039) do
     t.integer "cached_weighted_score", default: 0
     t.integer "cached_weighted_total", default: 0
     t.float "cached_weighted_average", default: 0.0
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,6 +59,7 @@ ActiveRecord::Schema.define(version: 20190901012039) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
